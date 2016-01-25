67807a26ac7a99fb85482d8a7dc0d55f66439540
Anton Rogachevskiy <tenkfm@gmail.com>
2016-01-25 14:27:18 +0100

    Star -> star image in assets

---
+++ EPPYK/Utils/EPPYK-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Project-Bridging-Header.h
+//  EPPYK
+//
+//  Created by Anton Rogachevskiy on 23/01/16.
+//  Copyright © 2016 Anton Rogachevskyi. All rights reserved.
+//
+
+#ifndef Project_Bridging_Header_h
+#define Project_Bridging_Header_h
+
+//#import “AFNetworking.h”
+
+
+#endif /* Project_Bridging_Header_h */
